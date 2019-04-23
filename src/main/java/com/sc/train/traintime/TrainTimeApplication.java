53cfb5a6e0a8d3f369c61a40bd516640c172c2f7
wky <1095077073@qq.com>
2019-04-23 15:01:00 +0800

    列车时刻表后台

---
+++ src/main/java/com/sc/train/traintime/TrainTimeApplication.java
@@ -0,0 +1,13 @@
+package com.sc.train.traintime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainTimeApplication.class, args);
+    }
+
+}
